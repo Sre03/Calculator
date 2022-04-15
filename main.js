feed43763c83fcc5b5477e067ee140ad71238d50
Sri <abhisrimohan03@gmail.com>
2022-04-15 14:31:43 +0530

    Fully working calculator ready

--- main.js
+++ main.js
@@ -15,8 +15,49 @@ inputs.forEach(inp => {
         else if(text == "DEL"){
             answer.value = answer.value.substr(0,answer.value.length -1);
         }
-        else if(text=="CE"){
-            
+        else if(text=="e"){
+            screenvalue += 2.718;
+            answer.value = screenvalue;
+        }
+        else if(text=="π"){
+            screenvalue += 3.141;
+            answer.value = screenvalue;
+        }
+        else if(text=="ln x"){
+            screenvalue = Math.log(eval(screenvalue));
+            answer.value = screenvalue;
+        }
+        else if(text=="1/x"){
+            screenvalue = 1/eval(screenvalue);
+            answer.value = screenvalue;
+        }
+        else if(text=="√x"){
+            screenvalue = eval(screenvalue)**(1/2);
+            answer.value = screenvalue;
+        }
+        else if(text=="+/-"){
+            screenvalue += "-";
+            answer.value = screenvalue;
+        }
+        else if(text=="sin x"){
+            screenvalue = Math.sin(eval(screenvalue));
+            answer.value = screenvalue;
+        }
+        else if(text=="cos x"){
+            screenvalue = Math.cos(eval(screenvalue));
+            answer.value = screenvalue;
+        }
+        else if(text=="tan x"){
+            screenvalue = Math.tan(eval(screenvalue));
+            answer.value = screenvalue;
+        }
+        else if(text=="e\u02E3"){
+            screenvalue = Math.exp(eval(screenvalue));
+            answer.value = screenvalue;
+        }
+        else if(text=="x\u00B2"){
+            screenvalue = (eval(screenvalue))**2;
+            answer.value = screenvalue;
         }
         else{
             screenvalue += text;
